9a8557e78f5472937b1e7b1d394fcff5bb93b525
Anja Svartberg <anjasvartberg@gmail.com>
2012-11-18 22:14:22 +0100

    Kommunikasjon med Telldus

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,15 @@
 var Pyntemp = {};
+Pyntemp.Telldus = {};
 
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response) {
-	console.log(request.url);
 	if (request.url == "/sensors") {
-		response.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
-		response.end(Pyntemp.init(), 'utf-8');
+		Pyntemp.Telldus.getSensorList(function (sensors){
+			response.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+			response.end(JSON.stringify(sensors), 'utf-8');			
+		});
 	} else {
 		fs.readFile('../frontend' + request.url, function(error, content) {
 	        if (error) {
@@ -23,21 +25,80 @@ http.createServer(function (request, response) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-Pyntemp.init = function() {
-	var sensors = new Pyntemp.Sensors();
-	sensors.push(new Pyntemp.Sensor({title: "Outside", temperature: "-2"}));
-	sensors.push(new Pyntemp.Sensor({title: "Kitchen", temperature: "5"}));
-	sensors.push(new Pyntemp.Sensor({title: "Livingroom", temperature: "8"}));
-	sensors.push(new Pyntemp.Sensor({title: "Bedroom", temperature: "6"}));
-	return JSON.stringify(sensors);
-}
-
 Pyntemp.Sensor = function (sensor) {
-	this.title = sensor.title;
-	this.temperature = sensor.temperature;
+	this.name = sensor.name;
+	this.temperature = sensor.temp;
+	this.humidity = sensor.humidity;
+	console.log(this.name + " " + this.temperature + " " + this.humidity);
 }
 
 Pyntemp.Sensors = function () {
+	this.sensors = new Array();
+	this.length = 0;
+	this.push = function(sensor) {
+		this.sensors.push(sensor);
+		this.length += 1;
+	} 
 }
 
-Pyntemp.Sensors.prototype = Array.prototype;
+Pyntemp.Telldus.getSensorList = function (callback) {
+	var util = require('util');
+	var path = "sensors/list";
+	var sensors = new Pyntemp.Sensors();
+	Pyntemp.Telldus.getAuthorization(function (){
+		Pyntemp.Telldus.getResource(path, function (data) {
+			console.log(data.sensor);
+			var listLength = data.sensor.length;
+			for (var sensor in data.sensor) {
+				console.log(data.sensor[sensor]);
+				Pyntemp.Telldus.getResource("sensor/info?id=" + data.sensor[sensor].id, function (data) {
+					console.log(data);
+					sensors.push(new Pyntemp.Sensor({name:data.name, temp: data.data[0].value, humidity: data.data[1].value }));
+					console.log(sensors.length + " " + listLength);
+					if (sensors.length == listLength) {
+						callback(sensors);	
+					}
+				});
+			}
+ 		});
+	});
+}
+
+
+
+Pyntemp.Telldus.getResource = function(path, callback){
+	var util = require('util');
+	Pyntemp.Telldus.oa.getProtectedResource("http://api.telldus.com/json/" + path, "GET", 
+		'token', //TODO: replace with oauth_access_token
+		'token secret',  //TODO: replace with oauth_access_token_secret
+		function (error, data, response) {
+			util.puts(data);
+			callback(JSON.parse(data));
+  		}
+	);	
+}
+
+Pyntemp.Telldus.getAuthorization = function(callback) {
+	var util = require('util');
+	var OAuth = require('oauth').OAuth;
+	
+	var oa = new OAuth("http://api.telldus.com/oauth/requestToken",
+	                  "http://api.telldus.com/oauth/accessToken",
+	                  "public key",
+	                  "private key",
+	                  "1.0",
+	                  null,
+	                  "HMAC-SHA1")
+
+	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+	  if(error) {
+	  	console.log(error)
+	  }
+	  else {
+	    oa.getOAuthAccessToken(oauth_token, oauth_token_secret, function(error, oauth_access_token, oauth_access_token_secret, results2) {
+	      Pyntemp.Telldus.oa = oa;
+	      callback();
+	    });
+	  }
+	})	
+}
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,17 +7,21 @@
     var el = $("#sensors");
     var view = new Pyntemp.SensorsView({sensors: sensors, el: el});
     sensors.fetch();
-  };
+  }
 
   Pyntemp.Sensors = Simple.Model.extend({
     dataType: "json",
     initialize: function() {
         this.url = "http://localhost:1337/sensors";
     }
-  });
+  })
 
   Pyntemp.SensorsView = Simple.View.extend({
-    template: 'heihei',
+    template: '<ul>' + 
+      '{{#sensors}}' +
+      '<li> {{name}} : {{temperature}} grader Celcius, {{humidity}} % luftfuktighet  </li>' +
+      '{{/sensors}}' +
+      '</ul>',
     initialize: function(options) {
       this.sensors = options.sensors;
       this.sensors.on("fetch:finished", this.render, this);
@@ -30,31 +34,4 @@
     }
   });
 
-/*
-  BEKK.User = Simple.Model.extend({
-    dataType: "jsonp",
-    initialize: function() {
-        this.url = "https://api.twitter.com/1/users/show.json?screen_name=anjasvartberg";
-    } 
-  });
-
-  BEKK.UserView = Simple.View.extend({
-    template: '<h2>{{name}}</h2>' +
-      '<img src="{{profile_image_url}}" alt="{{name}}">' +
-      '<ul>' +
-        '<li>Followers: <span class="followers">{{followers_count}}</span></li>' +
-        '<li>Following: <span class="following">{{friends_count}}</span></li>' +
-        '<li>Monologs: <span class="monologs">{{monologs}}</span></li>' +
-      '</ul>',
-	initialize: function(options) {
-		this.user = options.user;
-    this.user.on("fetch:finished", this.render, this);
-    this.el = options.el;
-	},
-  render: function() {
-    var html = Mustache.to_html(this.template, this.user.attrs());
-    this.el.html(html);
-  }
-  });
-*/
 })(Simple, Mustache);
